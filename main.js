f18d9fe9a4f5829f4efff3acc61cf77111eddbee
yassin alisheikh <yassinnawed@gmail.com>
2022-02-04 12:04:56 -0800

    add questtions

--- main.js
+++ main.js
@@ -1,12 +1,118 @@
-console.log("I'm connected");
+var questions = [
+    {
+        title: 'What is your name?',
+        correctAnswer: "sam",
+        answers: [
+            "john",
+            "sam",
+            "peter",
+            "kim"
+        ]
+    },
+    {
+        title: 'What is your age?',
+        correctAnswer: "22",
+        answers: [
+            "11",
+            "30",
+            "25",
+            "22"
+        ]
+    },
+    {
+        title: 'Where are you from?',
+        correctAnswer: "canada",
+        answers: [
+            "newyork",
+            "la",
+            "vegas",
+            "canada"
+        ]
 
-var imgTag = document.createElement("img");
+    },
 
-imgTag.setAttribute("src", "assets/images/hoopla-is-dead.png");
-imgTag.setAttribute("alt", "Start Test");
+]
 
-imgTag.addEventListener("click", function(){
-    console.log("Maybe this will start");
-});
+var startQuiz = document.getElementById("start-quiz");
+var starQuizSection = document.getElementById("start-section")
+var quizsection = document.getElementById("quiz-section")
 
-document.body.appendChild(imgTag);
+startQuiz.addEventListener("click",
+    function () {
+        starQuizSection.classList.add("hide");
+
+        quizsection.classList.remove("hide")
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("I'm connected");
+
+// var imgTag = document.createElement("img");
+
+// imgTag.setAttribute("src", "assets/images/hoopla-is-dead.png");
+// imgTag.setAttribute("alt", "Start Test");
+
+// imgTag.addEventListener("click", function(){
+//     console.log("Maybe this will start");
+// });
+
+// document.body.appendChild(imgTag);
